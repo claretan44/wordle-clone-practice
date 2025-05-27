10a2fe7f01d59df60eda4eb4d13fc389de24767a
cfsyj <cfsyjuco@gmail.com>
2025-05-27 17:08:56 +0800

    added code to move to new tiles and rows

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let currRow = 1;
 let currTile;
 let letterClass;
 let rowClass;
+let filledWord = false;
 
 
 function isLetter(letter)
@@ -19,15 +20,34 @@ document.addEventListener('DOMContentLoaded', function(event){
             letterClass = `letter-${currLetter}`;
             currTile = document.querySelector(`div.letter.${rowClass}.${letterClass}`);
             currTile.innerHTML = event.key;
+            if(currLetter===5)
+            {
+                filledWord = true;
+            }
+            else
+            {
+                currLetter += 1;
+            }
 
         }
         else
         {
             /*check for 'Enter', 'Backspace', and 'Shift' and 'CapsLock'*/
             /* TODO: allow backspace to go back to previous forms but NOT to submitted words*/
+            /* also make sure backspace resets filledword to false if needed */
             /* TODO: stop user from going to a succeeding word if a previous one hasn't been submitted */
             switch(event.key){
                 case 'Enter':
+                    if(filledWord)
+                    {
+                        /*TODO: function to submit word here */
+                        filledWord = false;
+                        if (currRow < 6)
+                        {
+                            currRow += 1;
+                            currLetter = 1;
+                        }
+                    } 
                     break;
                 case 'Backspace':
                     break;
